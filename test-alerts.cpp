1563073bc3b38aba53e15f55d1154565700fd0b8
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-05-24 18:08:10 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -13,4 +13,10 @@ TEST_CASE("classifies Temperature Breach") {
   REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING,10) == NORMAL);
   REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING,40) == TOO_HIGH);
   REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING,-10) == TOO_LOW);
+  REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING,10) == NORMAL);
+  REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING,50) == TOO_HIGH);
+  REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING,-10) == TOO_LOW);
+  REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING,10) == NORMAL);
+  REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING,45) == TOO_HIGH);
+  REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING,-10) == TOO_LOW);
 }
