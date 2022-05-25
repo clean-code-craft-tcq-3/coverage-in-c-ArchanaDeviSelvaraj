2b36dc30de0f5f65af0a5f32ddab9f1eb59db8ea
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-05-25 14:53:21 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -60,11 +60,11 @@ TEST_CASE("if cooling type not available") {
 TEST_CASE("checks and alerts if there is temperature breach") {
   BatteryCharacter batteryChar;
   batteryChar.coolingType = PASSIVE_COOLING;
-  REQUIRE(checkAndAlert(TO_CONTROLLER, batteryChar, -15) == SENT_TO_CONTROLLER);
-  checkAndAlert(TO_EMAIL, batteryChar, -15);
+  REQUIRE(checkAndAlert(TO_CONTROLLER, batteryChar, -15) == TOO_LOW);
+  REQUIRE(checkAndAlert(TO_EMAIL, batteryChar, -15) == SENT_TEMP_LOW_ALERT) ;
   batteryChar.coolingType = HI_ACTIVE_COOLING;
-  checkAndAlert(TO_CONTROLLER, batteryChar, 65);
-  checkAndAlert(TO_EMAIL, batteryChar, 65);
+  REQUIRE(checkAndAlert(TO_CONTROLLER, batteryChar, 65) == TOO_HIGH);
+  REQUIRE(checkAndAlert(TO_EMAIL, batteryChar, 65) == SENT_TEMP_HIGH_ALERT);
   batteryChar.coolingType = MED_ACTIVE_COOLING;
   checkAndAlert(TO_CONTROLLER, batteryChar, 25);
   checkAndAlert(TO_EMAIL, batteryChar, 25);
